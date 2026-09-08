74397efb506171155c1f058612f90bba97b2580a
Akhil <akhilbisht208@gmail.com>
2026-09-08 22:50:24 +0530

    practice

---
+++ loops/02_from_0_to_n_.c
@@ -0,0 +1,14 @@
+// print the number from 0 to n if n is given by user
+
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the value of n :");
+    scanf("%d", &n);
+    for(int i=0;i<=n;i++){
+        printf("%d ",i);
+    }
+    return 0;
+}
---
+++ loops/04-table_.c
@@ -0,0 +1,16 @@
+//print the table of a number input by the user
+
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the value of n : ");
+    scanf("%d",&n);
+    for(int i=1;i<=10;i++)
+    {
+        printf("%d X %d = %d\n",n,i,n*i);
+    }
+    return 0;
+
+}
